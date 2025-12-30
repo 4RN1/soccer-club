acb85b42864a9c61e4e0ac0d4d8bde4c95371e2a
anri <anriskr14@gmail.com>
2025-12-30 20:52:00 +0400

    path fix

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import FooterComp from "../components/FooterComp";
 import "../css/rulesPages.css";
-
+// dd
 function Note() {
   return (
     <>
